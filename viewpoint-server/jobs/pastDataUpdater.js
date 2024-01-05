e01043824aca01d156a732135927dc914d2abc9b
Shayan Halder <shayanhalder@Shayans-MacBook-Air.local>
2024-01-04 20:37:30 -0800

    add script termination to cron jobs

--- viewpoint-server/jobs/pastDataUpdater.js
+++ viewpoint-server/jobs/pastDataUpdater.js
@@ -1,6 +1,6 @@
 import { updatePastData } from "../app.js";
 
-console.log("Starting past data updater scheduled task: ")
-const pastData = await updatePastData()
-console.log("Database successfully updated with the following past news data: ", pastData)
-
+console.log("Starting past data updater scheduled task: ");
+const pastData = await updatePastData();
+console.log("Database successfully updated with the following past news data: ", pastData);
+process.exit();
