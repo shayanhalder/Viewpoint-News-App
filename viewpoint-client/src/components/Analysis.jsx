724e9ff2003efb2fcf761d403262506024f7885f
Shayan Halder <halders2@uci.edu>
2024-01-06 14:55:47 -0800

    fix bullet point formatting issues

--- viewpoint-client/src/components/Analysis.jsx
+++ viewpoint-client/src/components/Analysis.jsx
@@ -9,16 +9,45 @@ export default function Analysis({ text }) {
   console.log(analysis);
 
   for (let currentInfo of analysis) {
-    const bullets = currentInfo.split("\n- ");
+    let bullets = currentInfo.split("\n");
+    bullets = bullets.filter((element) => element.trim() !== "");
+    for (let i = 0; i < bullets.length; i++) {
+      // let currentBullet = bullets[i];
+      if (bullets[i].indexOf(" * ") !== -1) {
+        // sub-bullets exist
+        bullets[i] = bullets[i].split("*");
+        bullets[i] = bullets[i].filter((element) => element.trim() !== "" && element.trim() !== "-");
+      }
+    }
+    console.log("bullets: ");
+    console.log(bullets);
     let title = currentInfo.split(" ")[1];
-    title = title.slice(0, title.length - 1);
+    title = title.slice(0, title.indexOf("\n"));
     const template = (
       <div>
         <p>{title}</p>
-        <ul>
-          {bullets.slice(1).map((bullet) => (
-            <li key={Math.random()}>{bullet}</li>
-          ))}
+        <ul key={Math.random()}>
+          {bullets.slice(1).map((bullet) => {
+            if (Array.isArray(bullet)) {
+              return (
+                <>
+                  {/* <li key={Math.random()}> {bullet[0]} </li> */}
+                  <ul key={Math.random()}>
+                    {bullet.map((subBullet) => {
+                      if (subBullet.trim() == "") {
+                        return null;
+                      }
+                      return <li key={Math.random()}> {subBullet} </li>;
+                    })}
+                  </ul>
+                </>
+              );
+            } else if (bullet.trim() == "") {
+              return null;
+            } else {
+              return <li key={Math.random()}>{bullet.replace("-", "")}</li>;
+            }
+          })}
         </ul>
       </div>
     );
