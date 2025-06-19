d1aeb23a6bf1d56e4ed99a2adf1042544fab7cba
Shayan Halder <halders2@uci.edu>
2025-06-19 10:13:16 -0700

    update cron job file

--- backend/jobs/newsUpdater.ts
+++ backend/jobs/newsUpdater.ts
@@ -1,37 +1,42 @@
 import { getTrendingTopics } from "../services/topics";
 import { fetchCurrentData } from "../services/news";
-import currentNews from "../models/news";
+import { formatDate } from "../services/date";
+import NewsDate from "../models/newsdate.js";
 
-async function updatePresentData() {
+async function updateNewsData() {
   try {
     console.log("Starting news data database job... ");
     console.log("Current time:", new Date().toISOString());
 
     let allTrendingTopics = await getTrendingTopics(); // webscrape current trending news topics
     let newsObject = await fetchCurrentData(allTrendingTopics); // request news API with trending topics to get current news data
-    // some trending topics may have no stories (due to news API technicalities), so fetchCurrentData removes those trending topics.
     const usedTrendingTopics = Object.keys(newsObject); // all of the trending topics that actually have news data associated with them
 
-    let current = await currentNews.find(); // get mongoose schema to update database
-
-    // update current news with the new data on MongoDB database
-    current[0].updateOne({ trending: usedTrendingTopics, news: newsObject }, (err: string, result: string) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log("Successfully updated database with present data.\nResult: ", result);
-      }
+    const todayDate = formatDate(new Date(), 0);
+    const uploadObject = new NewsDate({
+      date: todayDate,
+      trending: usedTrendingTopics,
+      news: newsObject,
     });
+    
+    try {
+      const newNews = await uploadObject.save();
+      console.log("Successfully updated database with past news data: \n", newNews);
+    } catch (err: any) {
+      console.log({ message: err.message });
+      return { message: err.message };
+    }
 
     return newsObject;
-  } catch (err) {
-    console.log(err);
-    return { message: err };
+    
+  } catch (err: any) {
+      console.log(err);
+      return { message: err };
   }
 }
 
 if (require.main === module) { // only run when this file is executed directly 
-    updatePresentData();
+    updateNewsData();
 }
 
 
