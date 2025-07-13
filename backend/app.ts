a4ccc9778d2bc8386fc7f355f452df9e328e94e5
Shayan Halder <shayanhalder2389@gmail.com>
2025-07-12 22:17:18 -0700

    remove deprecated code

--- backend/app.ts
+++ backend/app.ts
@@ -4,7 +4,6 @@ import mongoose from "mongoose";
 import express from "express";
 import cors from "cors";
 import newsRouter from "./routes/news.js";
-// import currentNews from "./models/news";
 import newsDate from "./models/newsDate";
 import dotenv from "dotenv";
 
@@ -50,69 +49,3 @@ app.get("/current", async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 });
-
-/* Updates the MongoDB database with the trending topics at the end of a given day
-     and past news data about the trending topics from a variety of news sources on both left 
-     and right-wing media. */
-// export async function updatePastData() {
-//   console.log("Starting past news database updater scheduled task:");
-//   const trendingTopics = await getTrendingTopics();
-//   const todayDate = formatDate(new Date(), 1); // one day in advance since for some reason on the backend web hosting if you don't do this it goes one day in advance
-//   const pastDate = formatDate(new Date(), 3); // go 3 days in advance since sometimes no news data available for a 24 hr period, or none available at all
-
-//   const pastNewsData = await fetchPastData(trendingTopics, todayDate, pastDate);
-//   const usedTrendingTopics = Object.keys(pastNewsData);
-
-//   console.log("Past news data object:");
-//   console.log(pastNewsData);
-
-//   console.log("Used trending topics");
-//   console.log(usedTrendingTopics);
-
-//   const uploadObject = new NewsDate({
-//     date: todayDate,
-//     trending: usedTrendingTopics,
-//     news: pastNewsData,
-//   });
-
-//   try {
-//     const newNews = await uploadObject.save();
-//     console.log("Successfully updated database with past news data: ", newNews);
-//   } catch (err) {
-//     console.log({ message: err.message });
-//     return { message: err.message };
-//   }
-
-//   return pastNewsData;
-// }
-
-
-/* Updates the MongoDB database with the current trending topics and top stories about the 
-trending topics from a variety of news sources on both left and right-wing media. */
-
-// export async function updatePresentData() {
-//   try {
-//     console.log("Starting current news data database updater: ");
-//     let allTrendingTopics = await getTrendingTopics(); // webscrape current trending news topics
-//     let newsObject = await fetchCurrentData(allTrendingTopics); // request news API with trending topics to get current news data
-//     // some trending topics may have no stories (due to news API technicalities), so fetchCurrentData removes those trending topics.
-//     const usedTrendingTopics = Object.keys(newsObject); // all of the trending topics that actually have news data associated with them
-
-//     let current = await currentNews.find(); // get mongoose schema to update database
-
-//     // update current news with the new data on MongoDB database
-//     current[0].updateOne({ trending: usedTrendingTopics, news: newsObject }, (err, result) => {
-//       if (err) {
-//         console.log(err);
-//       } else {
-//         console.log("Successfully updated database with present data. Result: ", result);
-//       }
-//     });
-
-//     return newsObject;
-//   } catch (err) {
-//     console.log(err);
-//     return { message: err };
-//   }
-// }
-
--- backend/models/newsDate.ts
+++ backend/models/newsDate.ts
@@ -1,51 +1,6 @@
 import mongoose from 'mongoose'
 import { newsStorySchema } from './news'
 
-// const newsStorySchema = new mongoose.Schema({
-//     source: {
-//         type: Map,
-//         required: true,
-//         of: {
-//             id: {
-//                 type: String,
-//                 required: true
-//             },
-//             name: {
-//                 type: String,
-//                 required: true,
-//             }
-//         }
-//     },
-//     author: {
-//         type: String,
-//         required: true
-//     },
-//     title: {
-//         type: String,
-//         required: true
-//     },
-//     description: {
-//         type: String,
-//         required: true
-//     },
-//     url: {
-//         type: String,
-//         required: true
-//     },
-//     urlToImage: {
-//         type: String,
-//         required: true
-//     },
-//     publishedAt: {
-//         type: String,
-//         required: true
-//     },
-//     content: {
-//         type: String,
-//         required: true
-//     },
-// })
-
 const newsDateSchema = new mongoose.Schema({
     date: {
         type: String,
